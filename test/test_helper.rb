ff48841b23679dd8417dea48dffd4348710212d4
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2026-02-27 17:11:29 +0400

    update

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,17 +4,17 @@ require "rails/test_help"
 require_relative "test_helpers/session_test_helper"
 
 module ActiveSupport
-	class TestCase
-		# Run tests in parallel with specified workers
-		parallelize(workers: :number_of_processors)
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
-		# Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-		fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-		# Add more helper methods to be used by all tests here...
-		def redis_available?
-			# Try to connect to Redis, return true if ok
-			Redis.new.ping rescue false
-		end
-	end
+    # Add more helper methods to be used by all tests here...
+    def redis_available?
+      # Try to connect to Redis, return true if ok
+      Redis.new.ping rescue false
+    end
+  end
 end
