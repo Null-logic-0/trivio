32fe0d8846a27ec4a8bdb6cd9fc0d408d45c2954
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2026-02-27 15:41:57 +0400

    Remove scaffolded controller tests
    Remove placeholder scaffold-generated tests from TradesControllerTest and WatchlistsControllerTest. Deleted the basic "should get create" (trades, watchlists) and "should get destroy" (watchlists) tests since they were unused/redundant.

--- test/controllers/trades_controller_test.rb
+++ test/controllers/trades_controller_test.rb
@@ -1,8 +1,5 @@
 require "test_helper"
 
 class TradesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get trades_create_url
-    assert_response :success
-  end
+
 end
--- test/controllers/watchlists_controller_test.rb
+++ test/controllers/watchlists_controller_test.rb
@@ -1,13 +1,5 @@
 require "test_helper"
 
 class WatchlistsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get watchlists_create_url
-    assert_response :success
-  end
 
-  test "should get destroy" do
-    get watchlists_destroy_url
-    assert_response :success
-  end
 end
